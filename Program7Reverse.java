e37b9a4e1377c39bfa4a56a49d32cbc1adcdf46c
Krish Gudigar <krishgudigar2006@gmail.com>
2025-09-03 16:09:23 +0530

    Update and rename reverse.java to Program7Reverse.java

--- Program7Reverse.java
+++ Program7Reverse.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class reverse {
+public class Program7Reverse {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         System.out.println("Enter a number");
@@ -16,3 +16,4 @@ public class reverse {
     }
     
 }
+
